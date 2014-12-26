c3a04d2c0b019b0b1ee24e76c2957bdfc959942c
Bastien Nocera <hadess@hadess.net>
2014-12-26 23:33:27 +0100

    Remove DBG_DELAY_OS specific code

--- os_dep/osdep_service.c
+++ os_dep/osdep_service.c
@@ -757,38 +757,6 @@ void rtw_usleep_os(int us)
 		msleep( (us/1000) + 1);
 }
 
-#ifdef DBG_DELAY_OS
-void _rtw_mdelay_os(int ms, const char *func, const int line)
-{
-	#if 0
-	if(ms>10)
-		DBG_871X("%s:%d %s(%d)\n", func, line, __FUNCTION__, ms);
-		rtw_msleep_os(ms);
-	return;
-	#endif
-
-
-	DBG_871X("%s:%d %s(%d)\n", func, line, __FUNCTION__, ms);
-
-   	mdelay((unsigned long)ms); 
-}
-void _rtw_udelay_os(int us, const char *func, const int line)
-{
-
-	#if 0
-	if(us > 1000) {
-	DBG_871X("%s:%d %s(%d)\n", func, line, __FUNCTION__, us);
-		rtw_usleep_os(us);
-		return;
-	}
-	#endif 
-
-
-	DBG_871X("%s:%d %s(%d)\n", func, line, __FUNCTION__, us);
-
-      udelay((unsigned long)us); 
-}
-#else
 void rtw_mdelay_os(int ms)
 {
    	mdelay((unsigned long)ms); 
@@ -797,7 +765,6 @@ void rtw_udelay_os(int us)
 {
       udelay((unsigned long)us); 
 }
-#endif
 
 void rtw_yield_os()
 {
