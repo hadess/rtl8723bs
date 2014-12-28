7051f71526c67eafb2bd406e784ad5bcc87f7794
Bastien Nocera <hadess@hadess.net>
2014-12-28 14:31:11 +0100

    Use kernel's byte-swapping headers

--- include/rtw_byteorder.h
+++ include/rtw_byteorder.h
@@ -26,9 +26,9 @@
 #endif
 
 #if defined (CONFIG_LITTLE_ENDIAN)
-#include <byteorder/little_endian.h>
+#include <linux/byteorder/little_endian.h>
 #elif defined (CONFIG_BIG_ENDIAN)
-#  include <byteorder/big_endian.h>
+#  include <linux/byteorder/big_endian.h>
 #else
 #  error "Must be LITTLE/BIG Endian Host"
 #endif
