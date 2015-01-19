1666e220a0a12438e50d6ca55e1bd91338d21c8f
Cosimo Cecchi <cosimoc@gnome.org>
2015-01-18 19:24:56 -0800

    Remove always off CONFIG_WAKELOCK

--- os_dep/osdep_service.c
+++ os_dep/osdep_service.c
@@ -210,121 +210,50 @@ void rtw_sleep_schedulable(int ms)
 #define RTW_SUSPEND_TRAFFIC_LOCK_NAME "rtw_wifi_traffic"
 #define RTW_SUSPEND_RESUME_LOCK_NAME "rtw_wifi_resume"
 #define RTW_RESUME_SCAN_LOCK_NAME "rtw_wifi_scan"
-#ifdef CONFIG_WAKELOCK
-static struct wake_lock rtw_suspend_lock;
-static struct wake_lock rtw_suspend_ext_lock;
-static struct wake_lock rtw_suspend_rx_lock;
-static struct wake_lock rtw_suspend_traffic_lock;
-static struct wake_lock rtw_suspend_resume_lock;
-static struct wake_lock rtw_resume_scan_lock;
-#endif
 
 inline void rtw_suspend_lock_init(void)
 {
-	#ifdef CONFIG_WAKELOCK
-	wake_lock_init(&rtw_suspend_lock, WAKE_LOCK_SUSPEND, RTW_SUSPEND_LOCK_NAME);
-	wake_lock_init(&rtw_suspend_ext_lock, WAKE_LOCK_SUSPEND, RTW_SUSPEND_EXT_LOCK_NAME);
-	wake_lock_init(&rtw_suspend_rx_lock, WAKE_LOCK_SUSPEND, RTW_SUSPEND_RX_LOCK_NAME);
-	wake_lock_init(&rtw_suspend_traffic_lock, WAKE_LOCK_SUSPEND, RTW_SUSPEND_TRAFFIC_LOCK_NAME);
-	wake_lock_init(&rtw_suspend_resume_lock, WAKE_LOCK_SUSPEND, RTW_SUSPEND_RESUME_LOCK_NAME);
-	wake_lock_init(&rtw_resume_scan_lock, WAKE_LOCK_SUSPEND, RTW_RESUME_SCAN_LOCK_NAME);
-	#endif
 }
 
 inline void rtw_suspend_lock_uninit(void)
 {
-	#ifdef CONFIG_WAKELOCK
-	wake_lock_destroy(&rtw_suspend_lock);
-	wake_lock_destroy(&rtw_suspend_ext_lock);
-	wake_lock_destroy(&rtw_suspend_rx_lock);
-	wake_lock_destroy(&rtw_suspend_traffic_lock);
-	wake_lock_destroy(&rtw_suspend_resume_lock);
-	wake_lock_destroy(&rtw_resume_scan_lock);
-	#endif
 }
 
 inline void rtw_lock_suspend(void)
 {
-	#ifdef CONFIG_WAKELOCK
-	wake_lock(&rtw_suspend_lock);
-	#endif
-
-	#if  defined(CONFIG_WAKELOCK)
-	//DBG_871X("####%s: suspend_lock_count:%d####\n", __FUNCTION__, rtw_suspend_lock.stat.count);
-	#endif
 }
 
 inline void rtw_unlock_suspend(void)
 {
-	#ifdef CONFIG_WAKELOCK
-	wake_unlock(&rtw_suspend_lock);
-	#endif
-
-	#if  defined(CONFIG_WAKELOCK)
-	//DBG_871X("####%s: suspend_lock_count:%d####\n", __FUNCTION__, rtw_suspend_lock.stat.count);
-	#endif
 }
 
 inline void rtw_resume_lock_suspend(void)
 {
-	#ifdef CONFIG_WAKELOCK
-	wake_lock(&rtw_suspend_resume_lock);
-	#endif
-
-	#if  defined(CONFIG_WAKELOCK)
-	//DBG_871X("####%s: suspend_lock_count:%d####\n", __FUNCTION__, rtw_suspend_lock.stat.count);
-	#endif
 }
 
 inline void rtw_resume_unlock_suspend(void)
 {
-	#ifdef CONFIG_WAKELOCK
-	wake_unlock(&rtw_suspend_resume_lock);
-	#endif
-
-	#if  defined(CONFIG_WAKELOCK)
-	//DBG_871X("####%s: suspend_lock_count:%d####\n", __FUNCTION__, rtw_suspend_lock.stat.count);
-	#endif
 }
 
 inline void rtw_lock_suspend_timeout(u32 timeout_ms)
 {
-	#ifdef CONFIG_WAKELOCK
-	wake_lock_timeout(&rtw_suspend_lock, rtw_ms_to_systime(timeout_ms));
-	#endif
 }
 
 inline void rtw_lock_ext_suspend_timeout(u32 timeout_ms)
 {
-	#ifdef CONFIG_WAKELOCK
-	wake_lock_timeout(&rtw_suspend_ext_lock, rtw_ms_to_systime(timeout_ms));
-	#endif
-	//DBG_871X("EXT lock timeout:%d\n", timeout_ms);
 }
 
 inline void rtw_lock_rx_suspend_timeout(u32 timeout_ms)
 {
-	#ifdef CONFIG_WAKELOCK
-	wake_lock_timeout(&rtw_suspend_rx_lock, rtw_ms_to_systime(timeout_ms));
-	#endif
-	//DBG_871X("RX lock timeout:%d\n", timeout_ms);
 }
 
 
 inline void rtw_lock_traffic_suspend_timeout(u32 timeout_ms)
 {
-	#ifdef CONFIG_WAKELOCK
-	wake_lock_timeout(&rtw_suspend_traffic_lock, rtw_ms_to_systime(timeout_ms));
-	#endif
-	//DBG_871X("traffic lock timeout:%d\n", timeout_ms);
 }
 
 inline void rtw_lock_resume_scan_timeout(u32 timeout_ms)
 {
-	#ifdef CONFIG_WAKELOCK
-	wake_lock_timeout(&rtw_resume_scan_lock, rtw_ms_to_systime(timeout_ms));
-	#endif
-	//DBG_871X("resume scan lock:%d\n", timeout_ms);
 }
 
 /*
