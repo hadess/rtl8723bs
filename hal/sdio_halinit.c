488eb9054b2774e8e1e9159818200c41a7d5c632
Cosimo Cecchi <cosimoc@gnome.org>
2015-01-18 18:13:29 -0800

    Remove undefined CONFIG_CHECK_AC_LIFETIME option

--- hal/sdio_halinit.c
+++ hal/sdio_halinit.c
@@ -974,11 +974,6 @@ static u32 rtl8723bs_hal_init(PADAPTER padapter)
 
 #if defined(CONFIG_TX_MCAST2UNI)
 
-#ifdef CONFIG_CHECK_AC_LIFETIME
-	// Enable lifetime check for the four ACs
-	rtw_write8(padapter, REG_LIFETIME_EN, 0x0F);
-#endif	// CONFIG_CHECK_AC_LIFETIME
-
 #ifdef CONFIG_TX_MCAST2UNI
 	rtw_write16(padapter, REG_PKT_VO_VI_LIFE_TIME, 0x0400);	// unit: 256us. 256ms
 	rtw_write16(padapter, REG_PKT_BE_BK_LIFE_TIME, 0x0400);	// unit: 256us. 256ms
