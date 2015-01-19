4f7a912cdad2547757c4d3fc82007cb6d15784a2
Cosimo Cecchi <cosimoc@gnome.org>
2015-01-18 19:16:51 -0800

    Remove always off CONFIG_FCS_MODE

--- core/rtw_mlme_ext.c
+++ core/rtw_mlme_ext.c
@@ -5748,24 +5748,6 @@ static void rtw_mlmeext_disconnect(_adapter *padapter)
 	pmlmeext->cur_ch_offset = HAL_PRIME_CHNL_OFFSET_DONT_CARE;
 
 	set_channel_bwmode(padapter, pmlmeext->cur_channel, pmlmeext->cur_ch_offset, pmlmeext->cur_bwmode);
-#ifdef CONFIG_FCS_MODE
-	else
-	{
-		PADAPTER pbuddy_adapter;	
-		struct mlme_ext_priv *pbuddy_mlmeext;
-
-		if(EN_FCS(padapter))
-		{			
-			pbuddy_adapter = padapter->pbuddy_adapter;
-			pbuddy_mlmeext = &pbuddy_adapter->mlmeextpriv;
-		
-			rtw_hal_set_hwreg(padapter, HW_VAR_STOP_FCS_MODE, NULL);
-
-			//switch to buddy's channel setting if buddy is linked
-			set_channel_bwmode(padapter, pbuddy_mlmeext->cur_channel, pbuddy_mlmeext->cur_ch_offset, pbuddy_mlmeext->cur_bwmode);
-		}	
-	}
-#endif //!CONFIG_FCS_MODE
 
 	flush_all_cam_entry(padapter);
 
