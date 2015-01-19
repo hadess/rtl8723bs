36e141c4b05ddd96571a51b34197ed6d20c10a6d
Cosimo Cecchi <cosimoc@gnome.org>
2015-01-18 19:18:33 -0800

    Remove always off CONFIG_LED_HANDLED_BY_CMD_THREAD

--- core/rtw_mlme_ext.c
+++ core/rtw_mlme_ext.c
@@ -7418,11 +7418,6 @@ u8 led_blink_hdl(_adapter *padapter, unsigned char *pbuf)
 		return H2C_PARAMETERS_ERROR;
 
 	ledBlink_param = (struct LedBlink_param *)pbuf;
-
-	#ifdef CONFIG_LED_HANDLED_BY_CMD_THREAD
-	BlinkHandler((PLED_DATA)ledBlink_param->pLed);
-	#endif
-
 	return 	H2C_SUCCESS;
 }
 
