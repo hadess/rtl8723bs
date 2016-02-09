08e034501554e0927af5ad2af5e5f1d6cb5b3737
Barnabás Czémán <trabarni@gmail.com>
2016-02-09 09:48:33 +0100

    rtw_efuse.c: Fix TRAILING_STATEMENTS errors

--- core/rtw_efuse.c
+++ core/rtw_efuse.c
@@ -148,10 +148,14 @@ u8
 Efuse_CalculateWordCnts(u8 word_en)
 {
 	u8 word_cnts = 0;
-	if (!(word_en & BIT(0)))	word_cnts++; /*  0 : write enable */
-	if (!(word_en & BIT(1)))	word_cnts++;
-	if (!(word_en & BIT(2)))	word_cnts++;
-	if (!(word_en & BIT(3)))	word_cnts++;
+	if (!(word_en & BIT(0)))
+		word_cnts++; /*  0 : write enable */
+	if (!(word_en & BIT(1)))
+		word_cnts++;
+	if (!(word_en & BIT(2)))
+		word_cnts++;
+	if (!(word_en & BIT(3)))
+		word_cnts++;
 	return word_cnts;
 }
 
