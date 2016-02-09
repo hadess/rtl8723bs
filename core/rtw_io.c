406aeb53f5ea41e0f8572d9d9b8d9c027f22c089
Barnabás Czémán <trabarni@gmail.com>
2016-02-09 15:06:03 +0100

    rtw_io.c: Fix ASSIGN_IN_IF errors

--- core/rtw_io.c
+++ core/rtw_io.c
@@ -184,8 +184,8 @@ int rtw_init_io_priv(struct adapter *padapter, void (*set_intf_ops)(struct adapt
 int rtw_inc_and_chk_continual_io_error(struct dvobj_priv *dvobj)
 {
 	int ret = false;
-	int value;
-	if ((value = atomic_inc_return(&dvobj->continual_io_error)) > MAX_CONTINUAL_IO_ERR) {
+	int value = atomic_inc_return(&dvobj->continual_io_error);
+	if (value > MAX_CONTINUAL_IO_ERR) {
 		DBG_871X("[dvobj:%p][ERROR] continual_io_error:%d > %d\n", dvobj, value, MAX_CONTINUAL_IO_ERR);
 		ret = true;
 	} else {
