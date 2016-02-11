ab6656c7aa59dc5ab091b9a70b2b7b469655a52c
Barnabás Czémán <trabarni@gmail.com>
2016-02-11 18:22:35 +0100

    rtw_pwrctrl.c: Fix INLINE_LOCATION errors

--- core/rtw_pwrctrl.c
+++ core/rtw_pwrctrl.c
@@ -840,12 +840,12 @@ static void pwr_rpwm_timeout_handler(void *FunctionContext)
 	_set_workitem(&pwrpriv->rpwmtimeoutwi);
 }
 
-__inline static void register_task_alive(struct pwrctrl_priv *pwrctrl, u32 tag)
+static __inline void register_task_alive(struct pwrctrl_priv *pwrctrl, u32 tag)
 {
 	pwrctrl->alives |= tag;
 }
 
-__inline static void unregister_task_alive(struct pwrctrl_priv *pwrctrl, u32 tag)
+static __inline void unregister_task_alive(struct pwrctrl_priv *pwrctrl, u32 tag)
 {
 	pwrctrl->alives &= ~tag;
 }
