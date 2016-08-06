a1c170571088bf95d3b83df09314dae52c81cb35
Barnabás Czémán <trabarni@gmail.com>
2016-08-06 10:10:45 +0200

    HalHWImg8723B_MAC.c: Fix all errors

--- hal/HalHWImg8723B_MAC.c
+++ hal/HalHWImg8723B_MAC.c
@@ -16,40 +16,72 @@
 
 #include "odm_precomp.h"
 
-static bool
-CheckPositive(
-    PDM_ODM_T     pDM_Odm,
-    const u32  Condition1,
-    const u32  Condition2
-   )
+static bool CheckPositive(
+	PDM_ODM_T pDM_Odm, const u32 Condition1, const u32 Condition2
+)
 {
-	u8    _BoardType = ((pDM_Odm->BoardType & BIT4) >> 4) << 0 | /*  _GLNA */
-                           ((pDM_Odm->BoardType & BIT3) >> 3) << 1 | /*  _GPA */
-                           ((pDM_Odm->BoardType & BIT7) >> 7) << 2 | /*  _ALNA */
-                           ((pDM_Odm->BoardType & BIT6) >> 6) << 3 | /*  _APA */
-                           ((pDM_Odm->BoardType & BIT2) >> 2) << 4;  /*  _BT */
+	u8 _BoardType =
+		((pDM_Odm->BoardType & BIT4) >> 4) << 0 | /*  _GLNA */
+		((pDM_Odm->BoardType & BIT3) >> 3) << 1 | /*  _GPA */
+		((pDM_Odm->BoardType & BIT7) >> 7) << 2 | /*  _ALNA */
+		((pDM_Odm->BoardType & BIT6) >> 6) << 3 | /*  _APA */
+		((pDM_Odm->BoardType & BIT2) >> 2) << 4;  /*  _BT */
 
 	u32   cond1   = Condition1, cond2 = Condition2;
-	u32    driver1 = pDM_Odm->CutVersion       << 24 |
-		                pDM_Odm->SupportPlatform  << 16 |
-		                pDM_Odm->PackageType      << 12 |
-		                pDM_Odm->SupportInterface << 8  |
-		                _BoardType;
+	u32    driver1 =
+		pDM_Odm->CutVersion       << 24 |
+		pDM_Odm->SupportPlatform  << 16 |
+		pDM_Odm->PackageType      << 12 |
+		pDM_Odm->SupportInterface << 8  |
+		_BoardType;
 
-	u32    driver2 = pDM_Odm->TypeGLNA <<  0 |
-		                pDM_Odm->TypeGPA  <<  8 |
-		                pDM_Odm->TypeALNA << 16 |
-		                pDM_Odm->TypeAPA  << 24;
+	u32 driver2 =
+		pDM_Odm->TypeGLNA <<  0 |
+		pDM_Odm->TypeGPA  <<  8 |
+		pDM_Odm->TypeALNA << 16 |
+		pDM_Odm->TypeAPA  << 24;
 
-	ODM_RT_TRACE(pDM_Odm, ODM_COMP_INIT, ODM_DBG_TRACE,
-                ("===> [8812A] CheckPositive (cond1, cond2) = (0x%X 0x%X)\n", cond1, cond2));
-	ODM_RT_TRACE(pDM_Odm, ODM_COMP_INIT, ODM_DBG_TRACE,
-                ("===> [8812A] CheckPositive (driver1, driver2) = (0x%X 0x%X)\n", driver1, driver2));
+	ODM_RT_TRACE(
+		pDM_Odm,
+		ODM_COMP_INIT,
+		ODM_DBG_TRACE,
+		(
+			"===> [8812A] CheckPositive (cond1, cond2) = (0x%X 0x%X)\n",
+			cond1,
+			cond2
+		)
+	);
+	ODM_RT_TRACE(
+		pDM_Odm,
+		ODM_COMP_INIT,
+		ODM_DBG_TRACE,
+		(
+			"===> [8812A] CheckPositive (driver1, driver2) = (0x%X 0x%X)\n",
+			driver1,
+			driver2
+		)
+	);
 
-	ODM_RT_TRACE(pDM_Odm, ODM_COMP_INIT, ODM_DBG_TRACE,
-                ("	(Platform, Interface) = (0x%X, 0x%X)\n", pDM_Odm->SupportPlatform, pDM_Odm->SupportInterface));
-	ODM_RT_TRACE(pDM_Odm, ODM_COMP_INIT, ODM_DBG_TRACE,
-                ("	(Board, Package) = (0x%X, 0x%X)\n", pDM_Odm->BoardType, pDM_Odm->PackageType));
+	ODM_RT_TRACE(
+		pDM_Odm,
+		ODM_COMP_INIT,
+		ODM_DBG_TRACE,
+		(
+			"	(Platform, Interface) = (0x%X, 0x%X)\n",
+			pDM_Odm->SupportPlatform,
+			pDM_Odm->SupportInterface
+		)
+	);
+	ODM_RT_TRACE(
+		pDM_Odm,
+		ODM_COMP_INIT,
+		ODM_DBG_TRACE,
+		(
+			"	(Board, Package) = (0x%X, 0x%X)\n",
+			pDM_Odm->BoardType,
+			pDM_Odm->PackageType
+		)
+	);
 
 
 	/*  Value Defined Check =============== */
@@ -61,8 +93,8 @@ CheckPositive(
 		return false;
 
 	/*  Bit Defined Check ================ */
-    /*  We don't care [31:28] and [23:20] */
-    /*  */
+	/*  We don't care [31:28] and [23:20] */
+	/*  */
 	cond1   &= 0x000F0FFF;
 	driver1 &= 0x000F0FFF;
 
@@ -85,14 +117,12 @@ CheckPositive(
 	}
 	return false;
 }
-static bool
-CheckNegative(
-    PDM_ODM_T     pDM_Odm,
-    const u32  Condition1,
-    const u32  Condition2
-   )
+
+static bool CheckNegative(
+	PDM_ODM_T pDM_Odm, const u32 Condition1, const u32 Condition2
+)
 {
-    return true;
+	return true;
 }
 
 /******************************************************************************
@@ -206,70 +236,67 @@ static u32 Array_MP_8723B_MAC_REG[] = {
 
 };
 
-void
-ODM_ReadAndConfig_MP_8723B_MAC_REG(
-	PDM_ODM_T  pDM_Odm
-	)
+void ODM_ReadAndConfig_MP_8723B_MAC_REG(PDM_ODM_T pDM_Odm)
 {
-    u32     i         = 0;
-    u32     ArrayLen    = sizeof(Array_MP_8723B_MAC_REG)/sizeof(u32);
-    u32 *    Array       = Array_MP_8723B_MAC_REG;
+	u32 i = 0;
+	u32 ArrayLen = sizeof(Array_MP_8723B_MAC_REG)/sizeof(u32);
+	u32 *Array = Array_MP_8723B_MAC_REG;
 
-    ODM_RT_TRACE(pDM_Odm, ODM_COMP_INIT, ODM_DBG_LOUD, ("===> ODM_ReadAndConfig_MP_8723B_MAC_REG\n"));
+	ODM_RT_TRACE(
+		pDM_Odm,
+		ODM_COMP_INIT,
+		ODM_DBG_LOUD,
+		("===> ODM_ReadAndConfig_MP_8723B_MAC_REG\n")
+	);
 
-    for (i = 0; i < ArrayLen; i += 2)
-    {
-        u32 v1 = Array[i];
-        u32 v2 = Array[i+1];
+	for (i = 0; i < ArrayLen; i += 2) {
+		u32 v1 = Array[i];
+		u32 v2 = Array[i+1];
 
-        /*  This (offset, data) pair doesn't care the condition. */
-        if (v1 < 0x40000000)
-        {
-           odm_ConfigMAC_8723B(pDM_Odm, v1, (u8)v2);
-           continue;
-        }
-        else
-        {   /*  This line is the beginning of branch. */
-            bool bMatched = true;
-            u8  cCond  = (u8)((v1 & (BIT29|BIT28)) >> 28);
+		/*  This (offset, data) pair doesn't care the condition. */
+		if (v1 < 0x40000000) {
+			odm_ConfigMAC_8723B(pDM_Odm, v1, (u8)v2);
+			continue;
+		} else {
+			/*  This line is the beginning of branch. */
+			bool bMatched = true;
+			u8  cCond  = (u8)((v1 & (BIT29|BIT28)) >> 28);
 
-            if (cCond == COND_ELSE) { /*  ELSE, ENDIF */
-                bMatched = true;
-                READ_NEXT_PAIR(v1, v2, i);
-            } else if (! CheckPositive(pDM_Odm, v1, v2)) {
-                bMatched = false;
-                READ_NEXT_PAIR(v1, v2, i);
-                READ_NEXT_PAIR(v1, v2, i);
-            } else {
-                READ_NEXT_PAIR(v1, v2, i);
-                if (! CheckNegative(pDM_Odm, v1, v2))
-                    bMatched = false;
-                else
-                    bMatched = true;
-                READ_NEXT_PAIR(v1, v2, i);
-            }
+			if (cCond == COND_ELSE) { /*  ELSE, ENDIF */
+				bMatched = true;
+				READ_NEXT_PAIR(v1, v2, i);
+			} else if (!CheckPositive(pDM_Odm, v1, v2)) {
+				bMatched = false;
+				READ_NEXT_PAIR(v1, v2, i);
+				READ_NEXT_PAIR(v1, v2, i);
+			} else {
+				READ_NEXT_PAIR(v1, v2, i);
+				if (!CheckNegative(pDM_Odm, v1, v2))
+					bMatched = false;
+				else
+					bMatched = true;
+				READ_NEXT_PAIR(v1, v2, i);
+			}
 
-            if (bMatched == false)
-            {   /*  Condition isn't matched. Discard the following (offset, data) pairs. */
-                while (v1 < 0x40000000 && i < ArrayLen -2)
-                    READ_NEXT_PAIR(v1, v2, i);
+			if (bMatched == false) {
+				/*  Condition isn't matched. Discard the following (offset, data) pairs. */
+				while (v1 < 0x40000000 && i < ArrayLen-2)
+					READ_NEXT_PAIR(v1, v2, i);
 
-                i -= 2; /*  prevent from for-loop += 2 */
-            }
-            else /*  Configure matched pairs and skip to end of if-else. */
-            {
-                while (v1 < 0x40000000 && i < ArrayLen-2) {
-                    odm_ConfigMAC_8723B(pDM_Odm, v1, (u8)v2);
-                    READ_NEXT_PAIR(v1, v2, i);
-                }
+				i -= 2; /*  prevent from for-loop += 2 */
+			} else { /*  Configure matched pairs and skip to end of if-else. */
+				while (v1 < 0x40000000 && i < ArrayLen-2) {
+					odm_ConfigMAC_8723B(pDM_Odm, v1, (u8)v2);
+					READ_NEXT_PAIR(v1, v2, i);
+				}
 
-                /*  Keeps reading until ENDIF. */
-                cCond = (u8)((v1 & (BIT29|BIT28)) >> 28);
-                while (cCond != COND_ENDIF && i < ArrayLen-2) {
-                    READ_NEXT_PAIR(v1, v2, i);
-                    cCond = (u8)((v1 & (BIT29|BIT28)) >> 28);
-                }
-            }
-        }
-    }
+				/*  Keeps reading until ENDIF. */
+				cCond = (u8)((v1 & (BIT29|BIT28)) >> 28);
+				while (cCond != COND_ENDIF && i < ArrayLen-2) {
+					READ_NEXT_PAIR(v1, v2, i);
+					cCond = (u8)((v1 & (BIT29|BIT28)) >> 28);
+				}
+			}
+		}
+	}
 }
