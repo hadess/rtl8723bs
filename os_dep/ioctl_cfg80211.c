hadess/rtl8723bs

commit e71a5fc58c3a1092805c69e7bccb5ab2f6da38f9

rtl8723bs: Fix build on kernel 4.7
This patch is based on suggestions by Guido Serra.

Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>

os_dep/ioctl_cfg80211.c
@@ -1399,6 +1399,11 @@ exit:
 void rtw_cfg80211_indicate_scan_done(struct adapter *adapter, bool aborted)
 {
 	struct rtw_wdev_priv *pwdev_priv = adapter_wdev_data(adapter);
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 7, 0))
+	struct cfg80211_scan_info info = {
+		.aborted = aborted
+	};
+#endif
 
 	spin_lock_bh(&pwdev_priv->scan_req_lock);
 	if (pwdev_priv->scan_request != NULL) {
@@ -1413,7 +1418,11 @@ void rtw_cfg80211_indicate_scan_done(struct adapter *adapter, bool aborted)
 		}
 		else
 		{
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 7, 0))
+			cfg80211_scan_done(pwdev_priv->scan_request, &info);
+#else
 			cfg80211_scan_done(pwdev_priv->scan_request, aborted);
+#endif
 		}
 
 		pwdev_priv->scan_request = NULL;

commit 4d19f1757efd2e1e60ddcc959825d9841cc798cc

rtl8723bs: Fix error introduced with commit e71a5fc58c3a10
The changes in cfg80211_scan_done() are needed for kernel 4.8, not
4.7. Users of 4.7-git that include this change will need to wait
for 4.8-rc1.

Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>

os_dep/ioctl_cfg80211.c
@@ -1399,7 +1399,7 @@ exit:
 void rtw_cfg80211_indicate_scan_done(struct adapter *adapter, bool aborted)
 {
 	struct rtw_wdev_priv *pwdev_priv = adapter_wdev_data(adapter);
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 7, 0))
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 8, 0))
 	struct cfg80211_scan_info info = {
 		.aborted = aborted
 	};
@@ -1418,7 +1418,7 @@ void rtw_cfg80211_indicate_scan_done(struct adapter *adapter, bool aborted)
 		}
 		else
 		{
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 7, 0))
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 8, 0))
 			cfg80211_scan_done(pwdev_priv->scan_request, &info);
 #else
 			cfg80211_scan_done(pwdev_priv->scan_request, aborted);