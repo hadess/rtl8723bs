ec6b57203e665a7e56cdfa8be205fd2ae3e30e08
Icenowy Zheng <icenowy@aosc.xyz>
2017-03-06 08:54:19 +0800

    os_dep: include <linux/signal.h>
    <linux/signal.h> is now not implicitly included in kernel.
    
    Manually include it.
    
    Signed-off-by: Icenowy Zheng <icenowy@aosc.xyz>

--- include/osdep_service_linux.h
+++ include/osdep_service_linux.h
@@ -44,6 +44,7 @@
 	#include <linux/kthread.h>
 	#include <linux/list.h>
 	#include <linux/vmalloc.h>
+	#include <linux/signal.h>
 
 /* 	#include <linux/ieee80211.h> */
         #include <net/ieee80211_radiotap.h>
