a7ac50b4080c4bed803bd80759b9192bba3e65f0
Larry Finger <Larry.Finger@lwfinger.net>
2017-03-06 15:08:08 -0600

    rtl8723bs: Fix build errors for kernel 4.11
    The kernel includes no longer bring in <linux/sched/signal.h>. Include
    it explicitely.
    
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>

--- include/drv_types.h
+++ include/drv_types.h
@@ -22,6 +22,10 @@
 #ifndef __DRV_TYPES_H__
 #define __DRV_TYPES_H__
 
+#include <linux/version.h>
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 11, 0)
+#include <linux/sched/signal.h>
+#endif
 #include <autoconf.h>
 #include <basic_types.h>
 #include <osdep_service.h>
